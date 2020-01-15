1d8b18bfa03aee4328eacb2999306b54194a3a30
Crash2Overload <weigelt.e@gmail.com>
2020-01-15 14:05:29 +0100

    New Project

---
+++ ECDH_Pairing/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on 15. Januar 2020, 14:01
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
